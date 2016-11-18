5822c616d78fac50885878d95fe301b7c7e3f81a
Marcus Ofenhed <marcus@conditionraise.se>
2016-11-18 14:16:54 +0100

    Style changes and more reused code

--- Crypto/Pbkdf2.hs
+++ Crypto/Pbkdf2.hs
@@ -18,6 +18,18 @@ xorByteStrings x y
   | B.length x == B.length y = B.pack $ B.zipWith xor x y
   | otherwise = error "xor bytestrings are not of equal length"
 
+pbkdf2_internal createBlock prf password salt iterations = B.concat $ createBlock hash' first_iteration 1
+  where
+    hash' = prf password
+    first_iteration hash = additional_iterations hash hash 1
+    additional_iterations prev_hash prev_result i
+      | i == iterations = prev_result
+      | i > iterations = error "Count must be at least 1"
+      | otherwise = additional_iterations current_hash result (i + 1)
+        where
+          current_hash = (hash' prev_hash)
+          result = xorByteStrings current_hash prev_result
+
 -- | This is a non standard variation of PBKDF2 which recursively uses the
 -- last generated value to improve the salt. In difference to pbkdf2 the
 -- salt can not be precalculated for every iteration (with a simple append
@@ -42,20 +54,11 @@ pbkdf2_iterative :: (B.ByteString -> B.ByteString -> B.ByteString)
                  -> B.ByteString -- ^ @Salt@, the not neccesarily secret data to use in the PBKDF2 computations.
                  -> Integer -- ^ @c@, number of iterations for the the PBKDF2 computations.
                  -> B.ByteString -- ^ @DK@, the output data in the format of an unlimited lazy ByteString.
-pbkdf2_iterative prf password salt iterations = B.concat $ pbkdf2' (B.pack []) 1
+pbkdf2_iterative prf password salt iterations = pbkdf2_internal (createBlocks $ B.pack []) prf password salt iterations
   where
-    hash' = prf password
-    pbkdf2' :: B.ByteString -> Bin.Word32 -> [B.ByteString]
-    pbkdf2' i c = let prev = (pbkdf2'' (hash' $ B.concat [i, salt, B.pack $ octetsBE c]))
-                    in prev:(pbkdf2' (prf prev i) (c + 1))
-    pbkdf2'' hash = pbkdf2''' hash hash 1
-    pbkdf2''' prev_hash prev_result i
-      | i == iterations = prev_result
-      | i > iterations = error "Count must be at least 1"
-      | otherwise = pbkdf2''' current_hash result (i + 1)
-        where
-          current_hash = (hash' prev_hash)
-          result = xorByteStrings current_hash prev_result
+    createBlocks :: B.ByteString -> (B.ByteString -> B.ByteString) -> (B.ByteString -> B.ByteString) -> Bin.Word32 -> [B.ByteString]
+    createBlocks i hash iterate c = let prev = (iterate (hash $ B.concat [i, salt, B.pack $ octetsBE c]))
+                                      in prev:(createBlocks (prf prev i) hash iterate $ c + 1)
 
 pbkdf2 :: (B.ByteString -> B.ByteString -> B.ByteString)
            -- ^ @PRF@, the PRF function to be used for PBKDF2. The first
@@ -64,17 +67,8 @@ pbkdf2 :: (B.ByteString -> B.ByteString -> B.ByteString)
        -> B.ByteString -- ^ @Salt@, the not neccesarily secret data to use in the PBKDF2 computations.
        -> Integer -- ^ @c@, number of iterations for the the PBKDF2 computations.
        -> B.ByteString -- ^ @DK@, the output data in the format of an unlimited lazy ByteString.
-pbkdf2 prf password salt iterations = B.concat $ pbkdf2' 1 True
+pbkdf2 prf password salt iterations = pbkdf2_internal (createBlocks True) prf password salt iterations
   where
-    hash' = prf password
-    pbkdf2' :: Bin.Word32 -> Bool -> [B.ByteString]
-    pbkdf2' 1 False = error "Hashing algorithm looped, stopping to maintain security of data" -- Paranoia, but that's useful when doing crypto
-    pbkdf2' i _ = (pbkdf2'' (hash' $ B.concat [salt, B.pack $ octetsBE i])):(pbkdf2' (i + 1) False)
-    pbkdf2'' hash = pbkdf2''' hash hash 1
-    pbkdf2''' prev_hash prev_result i
-      | i == iterations = prev_result
-      | i > iterations = error "Count must be at least 1"
-      | otherwise = pbkdf2''' current_hash result (i + 1)
-        where
-          current_hash = (hash' prev_hash)
-          result = xorByteStrings current_hash prev_result
+    createBlocks :: Bool -> (B.ByteString -> B.ByteString) -> (B.ByteString -> B.ByteString) -> Bin.Word32 -> [B.ByteString]
+    createBlocks False _ _ 1 = error "Hashing algorithm looped, stopping to maintain security of data" -- Paranoia, but that's useful when doing crypto
+    createBlocks _ hash iterate i = (iterate (hash $ B.concat [salt, B.pack $ octetsBE i])):(createBlocks False hash iterate $ i + 1)

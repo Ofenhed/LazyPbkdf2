dd4e65872a99a3fc6fcdad5ad16f8e67e1736dd0
Marcus Ofenhed <marcus@conditionraise.se>
2017-04-22 13:49:10 +0200

    Corrected comment about how to verify the iterative pbkdf.

--- Crypto/Pbkdf2.hs
+++ Crypto/Pbkdf2.hs
@@ -8,7 +8,7 @@ import qualified Data.ByteString.Lazy as B
 import qualified Data.Binary as Bin
 
 octetsBE :: Bin.Word32 -> [Bin.Word8]
-octetsBE w = 
+octetsBE w =
     [ fromIntegral (w `shiftR` 24)
     , fromIntegral (w `shiftR` 16)
     , fromIntegral (w `shiftR` 8)
@@ -31,22 +31,20 @@ pbkdf2_internal createBlocks prf password salt iterations = B.concat $ createBlo
           current_hash = (hash' prev_hash)
           result = xorByteStrings current_hash prev_result
 
--- | This is a non standard variation of PBKDF2 which recursively uses the
--- last generated value to improve the salt. In difference to pbkdf2 the
--- salt can not be precalculated for every iteration (with a simple append
--- of 4 bytes), but has to be calculated for every single iteration. This
--- also creates a function where you cannot jump in the stream without
--- calculating everything before it.  Compared to the standard this
--- function only changes the salt for the initial PBKDF2 value of each
--- iteration to include a salt iterated from earlier parts of the PBKDF2
--- stream. This can be verified by removing the i from (hash $ B.concat
--- [blockSalt, salt, B.pack $ octetsBE c]).
+-- | This is a non standard variation of PBKDF2 which recursively uses the last
+-- generated value to improve the salt. In difference to pbkdf2 the salt can
+-- not be precalculated for every iteration (with a simple append of 4 bytes),
+-- but has to be calculated for every single iteration. This also creates a
+-- function where you cannot jump in the stream without calculating everything
+-- before it.  Compared to the standard this function only changes the salt for
+-- the initial PBKDF2 value of each iteration to include a salt iterated from
+-- earlier parts of the PBKDF2 stream. This can be verified by removing the
+-- blockSalt from (hash $ B.concat [blockSalt, salt, B.pack $ octetsBE c]).
 --
--- The added salt for the first iteration will be "", and all following
--- will be calculated as (PRF output input), where output is the output of
--- the previous block and input is the added salt for the previous block.
--- Notice that the output from the previous block is put in the password
--- filed of the PRF.
+-- The added salt for the first iteration will be "", and all following will be
+-- calculated as (PRF output input), where output is the output of the previous
+-- block and input is the added salt for the previous block.  Notice that the
+-- output from the previous block is put in the password filed of the PRF.
 pbkdf2_iterative :: (B.ByteString -> B.ByteString -> B.ByteString)
                      -- ^ @PRF@, the PRF function to be used for the
                      -- iterative PBKDF2. The first argument is secret, the
